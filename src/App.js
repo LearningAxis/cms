ed7960bee349b6a3e27de9c0117955b2d528d7d7
Learning Axis <118423320+LearningAxis@users.noreply.github.com>
2024-02-26 18:36:14 +0500

    testing new

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { defineOneEntry } from 'oneentry'
+
+const {Menus, Pages } = defineOneEntry('https://learningaxis.oneentry.cloud', {token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoieW91dHViZSIsInNlcmlhbE51bWJlciI6MSwiaWF0IjoxNzA4ODM3MzAwLCJleHAiOjE3NDAzNzMyODZ9.FTg2UWsVslqMHrInAnkuw6tTa1Xmb6rfUGekiH1JvMo', langCode:'my-langCode'})
+
+const value = await Pages.getPages('en_US');
+const svalue = await Menus.getMenusByMarker('menu');
 
 function App() {
+
+  console.log(value)
+  let a = svalue.pages
+  console.log(a)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {a.map((menu)=>(<a href={menu.pageUrl} key={menu.id}>{menu.localizeInfos.title}</a>))}
+    </>
   );
 }
 
